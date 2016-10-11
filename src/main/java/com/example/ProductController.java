00781339849578a9f6435c04ecdfcf39acb09e56
Alexander Koglin <alexander.koglin@capgemini.com>
2016-10-11 16:00:56 +0200

    Second initial commit

---
+++ src/main/java/com/example/ProductController.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/products")
+    public Product greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Product(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+}
